af354ec1e8fb94fef4e7c3016c315e2045446c63
Charles2Alvin <yggg100@126.com>
2019-11-04 14:15:30 +0100

    Adjust the directory

--- src/main/NovelReader.java
+++ src/main/NovelReader.java
@@ -1,5 +1,8 @@
+package main;
+
 public class NovelReader {
-    public String d() {
+    public String add() {
         return null;
     }
+
 }
--- src/main/Shingling.java
+++ src/main/Shingling.java
@@ -1,3 +1,5 @@
+package main;
+
 public class Shingling {
     private NovelReader novelReader;
     private void generate() {
