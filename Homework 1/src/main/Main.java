b0d78aba6547e49fa5f79ebbcd27fd541afae277
danielxsh <14211024@bjtu.edu.cn>
2019-11-10 16:45:06 +0100

    change Dataset

--- Homework 1/src/main/Main.java
+++ Homework 1/src/main/Main.java
@@ -20,30 +20,34 @@ public class Main {
         // 2nd::minhashing calculate jaccardSimilarity
         LinkedList<Set<Integer>> documents = new LinkedList<>();
         sets.forEach(s -> documents.add(s.getShingles()));
-        MinHashing minHashing = new MinHashing(documents, 1000, true);
+        int numPermutes = 1000;
+        MinHashing minHashing = new MinHashing(documents, numPermutes, true);
         float startTime = System.currentTimeMillis();
         int[][]signatureMatrix = minHashing.getSignByPermute();
 
-        // 3nd::LSH compare signature
-//        int bands = 50;
-//        int rows = 20;
-//        double threshold = 0.8;
-//        LSH lsh = new LSH(signatureMatrix, bands, rows, threshold);
-//        List<List<Integer>>candidatePairs = lsh.findCandidatePairs();
-//        List<List<Integer>>similarPairs = new ArrayList<>();
-//        for(List pair:candidatePairs){
-//            int index1 = Integer.parseInt(pair.get(0).toString());
-//            int index2 = Integer.parseInt(pair.get(1).toString());
+//        // 3nd::LSH compare signature
+        int bands = 50;
+        int rows = 20;
+        double threshold = 0.8;
+        LSH lsh = new LSH(signatureMatrix, bands, rows, threshold);
+        List<List<Integer>>candidatePairs = lsh.findCandidatePairs();
+        List<List<Integer>>similarPairs = new ArrayList<>();
+        for(List pair:candidatePairs){
+            int index1 = Integer.parseInt(pair.get(0).toString());
+            int index2 = Integer.parseInt(pair.get(1).toString());
 //            int[] array1 = signatureMatrix[index1];
 //            int[] array2 = signatureMatrix[index2];
 //            Set doc1 = new HashSet<>(Arrays.asList(array1));
 //            Set doc2 = new HashSet<>(Arrays.asList(array2));
-//            double similarity = compareSets.jaccardSimilarity(doc1, doc2);
-//            if(similarity>=threshold){
-//                similarPairs.add(pair);
-//            }
-//        }
-//        System.out.println("SimilarPairs are:"+similarPairs);
+            Set doc1 = sets.get(index1).getShingles();
+            Set doc2 = sets.get(index2).getShingles();
 
+            double similarity = compareSets.jaccardSimilarity(doc1, doc2);
+            System.out.println(similarity);
+            if(similarity>=threshold){
+                similarPairs.add(pair);
+            }
+        }
+        System.out.println("SimilarPairs are:"+similarPairs);
     }
 }
